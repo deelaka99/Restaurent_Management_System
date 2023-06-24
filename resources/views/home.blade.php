43e6d005c28fd4d34de79f43b5b55b2a16682323
deelaka99 <123wejith@gmail.com>
2023-06-24 13:06:11 +0530

    Changes done to the admin dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -87,7 +87,9 @@ https://templatemo.com/tm-558-klassy-cafe
                                 @if (Route::has('login'))
                                     <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                                     @auth
-                                    <li><a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a></li>
+                                    <li>
+                                        <x-app-layout></x-app-layout>
+                                    </li>
                                 @else
                                     <li><a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a></li>
 
@@ -100,7 +102,7 @@ https://templatemo.com/tm-558-klassy-cafe
                             </li>
                         </ul>        
                         <a class='menu-trigger'>
-                            <span>Menu</span>
+                            <!--<span>Menu</span>-->
                         </a>
                         <!-- ***** Menu End ***** -->
                     </nav>
--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,8 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home',[HomeController::class,"index"]);
+Route::get('/',[HomeController::class,"index"]);
+Route::get('/redirects',[HomeController::class,"redirects"]);
 
 Route::middleware([
     'auth:sanctum',
